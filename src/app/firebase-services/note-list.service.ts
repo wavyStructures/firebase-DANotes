b6357cecfac2611526399aeecf93bc6b0810ae25
wavyStructures <anja_schwab@gmx.de>
2024-08-19 22:21:22 +0200

    ways of read data still trouble with async on for ...

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore, collection } from '@angular/fire/firestore';
+import { Note } from '../interfaces/note.interface';
+import { Firestore, collectionData, collection, doc, onSnapshot } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 
@@ -7,9 +8,42 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class NoteListService {
+trashNotes: Note[] = [];
+normalNotes: Note[] = [];
 
+
+  unsubList;  
+  unsubSingle;
+
+  items$;
   firestore: Firestore = inject(Firestore);
 
+  constructor() { 
+    this.unsubList = onSnapshot(this.getNotesRef(), (list) => {
+      list.forEach(element => {
+      console.log(element);
+      })
+      });
+  
+      this.unsubSingle = onSnapshot(this.getSingleDocRef("notes", "AVQ20nwly6hPobaMCD0d"), (element) => {
+      // und hier kann man mit dem Element nun machen was man will
+      })
+    
+    
+    };
+  
 
-  constructor() { }
+    // this.items$ = collectionData(this.getNotesRef());
+    // }
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');}
+
+
+  getTrashRef() {
+      return collection(this.firestore, 'trash');}
+    
+getSingleDocRef(colId: string, docId: string){
+return doc(collection(this.firestore, 'collId'), docId);
+}
 }
